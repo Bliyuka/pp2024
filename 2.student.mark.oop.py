aee0af621dade7a7f39992ce3cc4be3dcf884a4e
Nguyễn Minh Tuấn <minhtuann903@gmail.com>
2024-01-20 01:45:20 +0700

    a lil bit of tweaking

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -5,18 +5,6 @@ class Student:
         self.__DoB = DoB
         self.__course = {}
 
-    # def set_id(self, id):
-    #     print(f"set STUDENT id to {id}")
-    #     self.__id = id
-
-    # def set_name(self, name):
-    #     print(f"set STUDENT name to {name}")
-    #     self.__name = name
-
-    # def set_DoB(self, DoB):
-    #     print(f"set STUDENT DoB to {DoB}")
-    #     self.__DoB = DoB
-
     def get_id(self):
         return self.__id
 
@@ -34,8 +22,7 @@ class Student:
 
     def get_mark(self,course_id):
         return self.__course[course_id]["mark"]
-    
-        
+
 class Course:
     def __init__(self, id, name):
         self.__id = id
@@ -43,27 +30,9 @@ class Course:
 
     def course_info(self):
         print(f"COURSE id: {self.__id} - {self.__name}")
-
-    
-    # def set_id(self, id):
-    #     self.__id = id
-    #     print(f"set COURSE id to {self.get_id()}")
-    
-    # def get_id(self):
-    #     return self.__id
-    
-    # def set_name(self, name):
-    #     self.__name = name
-    #     print(f"set COURSE name to {self.get_id()}")
         
     def get_name(self):
         return self.__name
-
-    # def add_course_info(self):
-    #     Cid = input("Enter COURSE id: ")
-    #     self.set_id(Cid)
-    #     Cname = input ("Enter COURSE name: ")
-    #     self.set_name(Cname)
 
 class Management_system(Course, Student):
     def __init__(self):
@@ -78,23 +47,33 @@ class Management_system(Course, Student):
         for i in self.__student:
             self.__student[i].add_course(c_id, c_name)
         
-    def add_mark(self, std_id, course_id, mark):
-        self.__student[std_id].add_mark(course_id, mark)
+    def add_mark(self, course_id):
+        for i in self.__student:
+            mark = input(f"Enter {self.__student[i].get_name()}'s mark: ")
+            self.__student[i].add_mark(course_id, mark)
         
     def show_mark(self, course_id):
         print(f"DISPLAYING STUDENT MARKS IN COURSE: {self.__course[course_id].get_name()}")
         for i in self.__student:
             print(f"{self.__student[i].get_name()}: {self.__student[i].get_mark(course_id)}")
-            
+
     def list_course(self):
+        print()
         print("SHOWING COURSE(S)")
+        x = 1
         for i in self.__course:
-            print(self.__course[i].get_name())
+            print(f"{x}) {self.__course[i].get_name()}")
+            x += 1
 
     def list_student(self):
+        print()
         print("SHOWING STUDENT(S)")
+        x = 1
         for i in self.__student:
-            print(self.__student[i].get_name())
+            print(f"{x}) {self.__student[i].get_name()}")
+            x += 1
+
+
 
 
 mark_management = Management_system()
@@ -116,16 +95,12 @@ for i in range(C_num):
     print()
     mark_management.add_course(C_id, C_name)
 
-# course_id = input("Enter a COURSE ID to input student marks: ")
-# for i in mark_management.__student:
-#     print(i)
-
-mark_management.add_mark("111", "11","19") #student_id - course_id - mark
-mark_management.add_mark("222", "11","20")
-mark_management.add_mark("111", "22","15")
+course_id = input("Enter a COURSE ID to input student marks: ")
+mark_management.add_mark(course_id)
 
 mark_management.list_course()
 
 mark_management.list_student()
 
-mark_management.show_mark("11")
+course_id = input("Enter a COURSE ID you want to see the marks: ")
+mark_management.show_mark(course_id)
